279bc03c34388206e2d39d99960c25eb72a67224
rolfosian <135147406+rolfosian@users.noreply.github.com>
2023-06-22 04:14:04 +1200

    Update gunicorn.conf.py

--- flask/gunicorn.conf.py
+++ flask/gunicorn.conf.py
@@ -1,9 +1,9 @@
-from glob import glob
 def post_worker_init(worker):
     from main import start_threads 
     start_threads()
 
-reload_extra_files = glob('*.json')
+worker_class = 'gthread'
+reload_extra_files = ['drgmissionsgod.json']
 reload = True
 threads = 12
 bind = '127.0.0.1:5000'
