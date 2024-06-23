4a96edb0cd20b97ce5a48221cb35339be165c704
rolfosian <135147406+rolfosian@users.noreply.github.com>
2024-06-23 18:14:04 +1200

    Update json

--- flask/drgmissionslib.py
+++ flask/drgmissionslib.py
@@ -1043,7 +1043,7 @@ def group_by_day_and_split_all(DRG):
         os.mkdir('./static/json/bulkmissions')
 
         for timestamp, dictionary in DRG.items():
-            dictionary['ver'] = 3
+            dictionary['ver'] = 4
             fname = timestamp.replace(':','-')
             with open(f'./static/json/bulkmissions/{fname}.json', 'w') as f:
                 json.dump(dictionary, f)
